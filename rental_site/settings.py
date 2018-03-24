c2d6d821fb7ba097b1f006bb0eff21be95e9e654
mashnoor3 <mashnoorrashid@gmail.com>
2018-03-24 14:45:18 -0400

    settings changes

--- rental_site/settings.py
+++ rental_site/settings.py
@@ -121,8 +121,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "rental_site/media")
