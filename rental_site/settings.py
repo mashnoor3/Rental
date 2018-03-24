2a29a9c5ede4124dcc534d5499928c40f47ef3c8
mashnoor3 <mashnoorrashid@gmail.com>
2018-03-24 17:26:16 -0400

    align settings with pythonanywhere

--- rental_site/settings.py
+++ rental_site/settings.py
@@ -129,9 +129,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "rental_site/media")
 
